efeed2c8c93a50a71de72421b5c74f6536a14a6a
Ariful islam <afkgamingno1@gmail.com>
2024-09-29 05:53:57 +0600

    add file

--- js.js
+++
@@ -1,7 +0,0 @@
-var type = new Typed(".multiple_text", {
-    strings: ["Frontend Developer", "Web Designer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    typeDaly: 1000,
-    loop: true
-})
---
+++ script.js
@@ -0,0 +1,17 @@
+
+    var type = new Typed(".multiple_text", {
+    strings: ["Frontend Developer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    typeDaly: 1000,
+    loop: true
+})
+
+window.onscroll = () => {
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('ariful', window.scrollY > 100);
+}
+
+
